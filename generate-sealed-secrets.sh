f5b85e230a32e5f0974574a09d99a0ef295be056
jiho lee <optional.int@kakao.com>
2022-06-06 05:13:58 +0900

    feat: route 53 secrets generation script

--- generate-sealed-secrets.sh
+++ generate-sealed-secrets.sh
@@ -1,4 +1,7 @@
 kubectl create secret generic application-secret --dry-run=client --from-env-file=./secrets.env -o json > secrets.json
 kubeseal --controller-name sealed-secrets --controller-namespace default --scope cluster-wide -o yaml < secrets.json > application-sealed-secret.yaml
 
-mv application-sealed-secret.yaml ./backend/templates/
+kubectl create secret generic route53-secrets --dry-run=client --from-env-file=./route53-secrets.env -n cert-manager -o json > route53-secrets.json
+kubeseal --controller-name sealed-secrets --controller-namespace default --scope cluster-wide -o yaml < route53-secrets.json > route53-secrets.yaml
+
+mv application-sealed-secret.yaml route53-secrets.yaml ./backend/templates/
