1c76eee99cf3676c9f30a478fbd4617a4549fba6
Jiho Lee <optional.int@kakao.com>
2022-07-01 17:15:24 +0000

    chore: remove dns01 extraArgs from cert manager

--- install-cert-manager.sh
+++ install-cert-manager.sh
@@ -5,5 +5,4 @@ helm install backend \
     --version v1.8.2 \
     --set ingressShim.defaultIssuerKind=ClusterIssuer \
     --set ingressShim.defaultIssuerName=letsencrypt-staging-issuer \
-    --set extraArgs='{--dns01-recursive-nameservers-only,--dns01-self-check-nameservers=8.8.8.8:53\,1.1.1.1:53}' \
     --set installCRDs=true jetstack/cert-manager
